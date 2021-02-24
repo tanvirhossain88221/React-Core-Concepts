6a6d2e430f1269c07dbcf5e63a18781949f4ca0a
Tanvir Hossain Robin <tanvirhossain88221@gmail.com>
2021-02-24 09:30:02 +0600

    send array object to props

--- src/App.js
+++ src/App.js
@@ -2,24 +2,93 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const products = [
+    {name:'Photoshop', price: '$90.99'},
+    {name:'Illustrator', price: '$60.99'},
+    {name:'PDF Reader', price: '$6.99'}
+  ]
+  // var name= "RoBIn";
+  // var person = {
+  //   Name: "tanvir",
+  //   job: "Web Developer"
+  // };
+
+  // //css add
+  // //react a class likte hole className diye likte hoy.
+  // var style = {
+  //   color: 'red',
+  //   // - dewa jabe na camel case use korte hobe like backgound-color> backgroundColor
+  //   backgroundColor: 'skyblue'
+  // }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>I'am a React Person</p>
+
+        <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product>
+
+        <Person name="Tanvir" job="web-Hero"></Person>
+        <Person name="Robin" food="Biriani"></Person>
+        <Person name="Tony Stark" food="Alcohol" job="CEO"></Person>
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
+        <h1>My Heading: {(2+5)*25+65}</h1>
+        <h1> Dynamic Name: {name}</h1> */}
+
+        {/* css add
+        <h3 className="" style= {style}> info: {person.Name +' '+ person.job}</h3> */}
+
+        {/* in line css in react */}
+        {/* <p style={{color:"yellow", backgroundColor: "violet"}}>my first react paragraph</p> */}
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
+}
+function Product(props) {
+  const productStyle={
+    border: '1px solid gray',
+    borderRadius: '5px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '200px',
+    float: 'left'
+  }
+  const {name, price} = props.product;
+  return (
+    <div style={productStyle}>
+      <h3>{name}</h3>
+      <h2>{price}</h2>
+      <button>Buy Now</button>
+    </div>
+  )
+}
+function Person(props) {
+  const personStyle ={
+    border: '2px solid red',
+    margin: '10px',
+    width: '400px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1>Name: {props.name}</h1>
+      <h3>Job: {props.job}</h3>
+      <p>Food: {props.food}</p>
+    </div>
+  )
 }
 
 export default App;
